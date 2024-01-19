2d5a525e40f56dfcfc9b4808b5a64da7048b6b59
tesfie056 <143583774+tesfie056@users.noreply.github.com>
2024-01-19 13:08:39 -0500

    I have added new file called random.c

---
+++ random.c
@@ -0,0 +1,13 @@
+//
+//  random.c
+//  testRandonNumber
+//
+//  Created by Tesfaldet Haileab on 1/18/24.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int randchar() {
+    char array[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' ,'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w','x', 'y', 'z', '\0'};
+    return array[rand() % 26];
+}
